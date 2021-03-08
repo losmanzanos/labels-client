1788d77b73f749f7af4328706436857eb2d0cf77
Chad Moravec <moravec@colorado.edu>
2021-03-08 14:27:38 -0700

    Fixed API Endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const config = {
   KEY: "AIzaSyBGci3Hqp0c1E74nQY9-czSUSA0X8RSKNo",
-  API_ENDPOINT: "http://localhost:9001",
+  // API_ENDPOINT: "http://localhost:9001",
+  API_ENDPOINT: "https://image-analyzer-server.herokuapp.com/",
   TOKEN_KEY: "myKey",
 };
 
